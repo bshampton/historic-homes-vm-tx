5a8cc2d713373751340144daf0edbb51591363c3
bill shampton <bill@shampton.net>
2012-06-17 16:02:37 -0500

    update default domain

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,5 +65,5 @@ MsgAttArtsfest::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
-  config.action_mailer.default_url_options = { :host => 'msg-att-artfest.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'growing-mist-4712.herokuapp.com' }
 end
